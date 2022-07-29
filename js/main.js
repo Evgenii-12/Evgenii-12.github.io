1577c99c7e526f4e55f0c1771721fba7a42f4b97
Evgenii-12 <110045218+Evgenii-12@users.noreply.github.com>
2022-07-29 21:43:17 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ $("body,html").animate({
 scrollTop:$("#" + $(this).data('value')).offset().top
 },1000)
 })
+
+function check() {
+	let num1 = Number(document.getElementById("min").value);
+	let num2 = Number(document.getElementById("max").value);
+    var x = Math.floor(num1 + Math.random() * (num2 - num1 + 1));
+document.querySelector('.result').innerText= x;
+console.log (num1, num2, x)
+
+}
